3a14327cce01ec2ea54bd022b2252071d928ea60
shinpei-hattori <hatto08101127@gmail.com>
2020-06-28 19:16:52 +0900

    再びphpのDB設定を変更

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,19 @@
 try{
 
     //  tryの中に記述
-        $db = new PDO('mysql:dbname=heroku_5959409df845877;host=us-cdbr-east-02.cleardb.com;port=3306;charset=utf8','b83e51344a27d3','5ef371b3');
+    $db = parse_url($_SERVER['mysql://b83e51344a27d3:5ef371b3@us-cdbr-east-02.cleardb.com/heroku_5959409df845877?reconnect=true
+    ']);
+    $db['heroku_5959409df845877'] = ltrim($db['path'], '/');
+    $dsn = "mysql:host={$db['us-cdbr-east-02.cleardb.com']};dbname={$db['heroku_5959409df845877']};charset=utf8";
+    $user = $db['b83e51344a27d3'];
+    $password = $db['5ef371b3'];
+    $options = array(
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+    );
+
+    $dbh = new PDO($dsn,$user,$password,$options);
     } catch (PDOException $e){
     //　例外処理を記述
         echo 'DB接続エラー:' . $e->getMessage();
