00de0fc55eac5f905ff4f9880871d504d570973b
shinpei-hattori <hatto08101127@gmail.com>
2020-06-28 18:46:21 +0900

    db情報を変更

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 try{
 
     //  tryの中に記述
-        $db = new PDO('mysql:dbname=movies_db;host=localhost;port=8889;charset=utf8','root','root');
+        $db = new PDO('mysql:dbname=heroku_5959409df845877;host=us-cdbr-east-02.cleardb.com;port=3306;charset=utf8','b83e51344a27d3','5ef371b3');
     } catch (PDOException $e){
     //　例外処理を記述
         echo 'DB接続エラー:' . $e->getMessage();
